8e8d01205f126fead38a34792aa56b7831b9ba40
이 성 노 <kulbo0926@gmail.com>
2022-03-11 16:15:15 +0900

    잉시 Callback 확인

--- app/src/main/java/kr/co/smartsoft/keepthetime_20220311/SignInActivity.kt
+++ app/src/main/java/kr/co/smartsoft/keepthetime_20220311/SignInActivity.kt
@@ -2,9 +2,14 @@ package kr.co.smartsoft.keepthetime_20220311
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kr.co.smartsoft.keepthetime_20220311.api.APIList
 import kr.co.smartsoft.keepthetime_20220311.api.ServerAPI
 import kr.co.smartsoft.keepthetime_20220311.databinding.ActivitySignInBinding
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class SignInActivity : BaseActivity() {
@@ -18,10 +23,24 @@ class SignInActivity : BaseActivity() {
 
     }
     override fun setupEvents() {
-        val retrofit = ServerAPI.getRetrofit()
-        val apiList = retrofit.create(APIList::class.java)
+//        val retrofit = ServerAPI.getRetrofit()
+//        val apiList = retrofit.create(APIList::class.java)
+//
+//        apiList.postRequestLogin("아이디", "aa")
+        val inputEmail = binding.edtId.text.toString()
+        val inputPassword = binding.edtPassword.text.toString()
 
-        apiList.postRequestLogin("아이디", "aa")
+        apiList.postRequestLogin(inputEmail, inputPassword).enqueue(object : Callback<JSONObject>{
+            override fun onResponse(call: Call<JSONObject>, response: Response<JSONObject>) {
+                Log.d("응답확인"., response.message())
+
+            }
+
+            override fun onFailure(call: Call<JSONObject>, t: Throwable) {
+
+            }
+
+        })
     }
 
     override fun setValues() {
