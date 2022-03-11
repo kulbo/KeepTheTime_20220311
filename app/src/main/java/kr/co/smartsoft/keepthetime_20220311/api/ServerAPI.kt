4a903591bc3f37be1c272eca8e1004bb3b974a87
이 성 노 <kulbo0926@gmail.com>
2022-03-11 15:44:57 +0900

    JSON 라이브러리

--- app/src/main/java/kr/co/smartsoft/keepthetime_20220311/api/ServerAPI.kt
+++ app/src/main/java/kr/co/smartsoft/keepthetime_20220311/api/ServerAPI.kt
@@ -1,6 +1,7 @@
 package kr.co.smartsoft.keepthetime_20220311.api
 
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ServerAPI {
 //    Retrofit 클래스 객체화 함수 => 단일 객체만 만들어서 모든 화면에서 공유
@@ -14,10 +15,12 @@ class ServerAPI {
         fun getRetrofit() : Retrofit {
 
 //            Retrofit 라이브러리는 클래스 차원에서 BASE_URL 을 설정할 수 있게 도와줌.
+//
 
             if (retrofit == null) {
                 retrofit = Retrofit.Builder()
-                    .baseUrl(BASE_URL)
+                    .baseUrl(BASE_URL)      // 어느 서버를 기
+                    .addConverterFactory(GsonConverterFactory.create()) // gson 라이브러리와 결합
                     .build()
             }
 
